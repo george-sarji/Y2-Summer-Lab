8cf5b554ae03b423658aaefb0cebea0088f04493
george17-meet <george17@meet.mit.edu>
2018-08-01 13:29:01 +0300

    Javascript lab 2 1.1 complete

--- JS Lab 2/object-literals/object-literals.js
+++ JS Lab 2/object-literals/object-literals.js
@@ -1,1 +1,12 @@
 // Create your 'me' object literal here!
+var me = {
+    firstName: "George",
+    lastName: "Sarji",
+    age: 17.8,
+    favoriteFoods: ["Literally", "Everything"],
+    picture: "Not needed..."    
+}
+
+
+var json = JSON.stringify(me);
+document.write(json);
