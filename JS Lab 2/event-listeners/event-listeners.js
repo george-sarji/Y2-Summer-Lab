e7bec72c240dd474a01ab42e400d3e78e87de689
george-sarji <georgesarji@gmail.com>
2018-08-02 16:08:04 +0300

    Javascript lab 2 1.5 complete, no images uploaded though.

--- JS Lab 2/event-listeners/event-listeners.js
+++ JS Lab 2/event-listeners/event-listeners.js
@@ -1,13 +1,16 @@
 // YOUR TASK: Add more pictures!
 var pictures = ['./imgs/dog.jpg'];
 var currentIndex = 0;
+document.getElementsByTagName("body")[0].addEventListener("click", showNextPicture);
 
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
-  }
 
+  }
+  alert("Clicked");
+  document.getElementsByTagName("img")[0].src = pictures[currentIndex];
   // YOUR TASK: Finish this function!
 }
