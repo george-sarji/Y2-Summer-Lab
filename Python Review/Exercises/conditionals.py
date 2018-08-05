8746d1e8bf4bce9f07f60a8775c4c6cdbc041e2a
george-sarji <george17@meet.mit.edu>
2018-08-05 18:12:01 +0300

    Python review exercise 1.2 complete

--- Python Review/Exercises/conditionals.py
+++ Python Review/Exercises/conditionals.py
@@ -1,1 +1,18 @@
 # Write your solution for 1.2 here!
+
+sum = 0
+for i in range(100):
+    if(i%2==0):
+        sum+=i
+
+print(sum)
+
+
+
+# Extra
+i=1000
+while(i>0):
+    if(i%6 == 2 and (i*i*i) % 5 ==3):
+        print(i)
+        break
+    i-=1
