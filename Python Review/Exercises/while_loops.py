99e2f9438a2048ccf3130b99a2b45e63a7437ccf
george-sarji <george17@meet.mit.edu>
2018-08-05 18:14:07 +0300

    Python review exercise 1.3 complete

--- Python Review/Exercises/while_loops.py
+++ Python Review/Exercises/while_loops.py
@@ -1,1 +1,17 @@
 # Write your solution for 1.3 here!
+index = 1
+sum = 0
+while(sum<10000):
+    sum+=index
+    index+=1
+print(index)
+
+
+
+# Extra
+index = 2
+sum = 0
+while(sum<10000):
+    sum+=index
+    index+=2
+print(index)
